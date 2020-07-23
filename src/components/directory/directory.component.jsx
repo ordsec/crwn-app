dfc4601a849a0b77307fc6bcec1671df3247e2e1
David Ordovskiy <david.ordovsky@gmail.com>
2020-07-23 12:04:25 -0400

    bootstrap directory and menu item components to render properly

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import MenuItem from '../menu-item/menuItem.component';
+
 import './directory.styles.scss';
 
 export default class Directory extends Component {
@@ -35,5 +37,21 @@ export default class Directory extends Component {
         }
       ]
     }
+  }
+
+  render() {
+    return (
+      <div className='directory-menu'>
+        {
+          this.state.sections.map(({ title, imageUrl, id }) => (
+            <MenuItem 
+              key={id}
+              title={title}
+              url={imageUrl}
+            />
+          ))
+        }
+      </div>
+    );
   }
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,41 +1,12 @@
 import React from 'react';
 
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='homepage'>
-    <div className='directory-menu'>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>LENSES</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>FILM</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>ACCESSORIES</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>DIGITAL CAMERAS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-      <div className='menu-item'>
-        <div className='content'>
-          <h1 className='title'>FILM CAMERAS</h1>
-          <span className='subtitle'>SHOP NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory />
   </div>
 );
 
