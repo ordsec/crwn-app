4107e3874bb7ab5e7ef0bbea08ec1cfdee2b516f
David Ordovskiy <david.ordovsky@gmail.com>
2020-07-28 10:33:29 -0400

    add the sign in/sign up component and route

---
+++ src/pages/sign-in-sign-up/signInSignUp.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './signInSignUp.styles.scss';
+
+const SignInSignUp = () => (
+  <div className='sign-in-and-sign-up'>
+    SIGN IN
+  </div>
+);
+
+export default SignInSignUp;
