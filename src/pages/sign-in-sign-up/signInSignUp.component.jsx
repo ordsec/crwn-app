91973cce8f0857c9fdceb27f961146edcbd81b66
David Ordovskiy <david.ordovsky@gmail.com>
2020-07-28 10:46:42 -0400

    create sign-in component with form, display it

--- src/pages/sign-in-sign-up/signInSignUp.component.jsx
+++ src/pages/sign-in-sign-up/signInSignUp.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import SignIn from '../../components/sign-in/signIn.component';
+
 import './signInSignUp.styles.scss';
 
 const SignInSignUp = () => (
   <div className='sign-in-and-sign-up'>
     SIGN IN
+    <SignIn />
   </div>
 );
 
