0563284205f80b5a1278858bd4cd08abaccdefa7
David Ordovskiy <david.ordovsky@gmail.com>
2020-07-30 13:49:33 -0400

    add firebase and configure sign-in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyCUcYqEZbGXiJaUNHBRPLCPlJf1MKmf1YM",
+  authDomain: "crwn-photo-db.firebaseapp.com",
+  databaseURL: "https://crwn-photo-db.firebaseio.com",
+  projectId: "crwn-photo-db",
+  storageBucket: "crwn-photo-db.appspot.com",
+  messagingSenderId: "177242670928",
+  appId: "1:177242670928:web:e7cab4891c2f5a53cace55",
+  measurementId: "G-FJ9ZGR7YXS"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+// make the google auth pop-up appear
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
