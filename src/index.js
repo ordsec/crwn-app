cfe9d761cc1d3a4d5b75d2ca27b13ce6dc4f1c98
David Ordovskiy <david.ordovsky@gmail.com>
2020-08-04 18:15:32 -0400

    add redux, set up root reducer and user reducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// gives access to the redux store
+import { Provider } from 'react-redux';
 
 import App from './App';
 
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
