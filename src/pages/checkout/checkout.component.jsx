4cc1b7d2826496761e72eee787feffeb8f28d174
David Ordovskiy <david.ordovsky@gmail.com>
2020-08-12 19:52:34 -0400

    create the checkout page and the route for it

---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './checkout.styles.scss';
+
+const CheckoutPage = () => (
+  <div>
+    <h2>Check this sh!t out!</h2>
+  </div>
+);
+
+export default CheckoutPage;
