e06b4af08217300f35f57bfe1fcefffc4620fb36
David Ordovskiy <david.ordovsky@gmail.com>
2020-08-17 08:19:16 -0400

    add redux-persist functionality to the app

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 // gives access to the redux store
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import App from './App';
 
@@ -13,7 +14,9 @@ import './index.css';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
