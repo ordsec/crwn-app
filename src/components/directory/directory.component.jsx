726c9642d0822fb2652ff7402a3274a0aacbdbee
David Ordovskiy <david.ordovsky@gmail.com>
2020-08-17 11:38:17 -0400

    connect directory component to redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,58 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import MenuItem from '../menu-item/menuItem.component';
 
+import { selectDirSections } from '../../redux/directory/directory.selectors';
+
 import './directory.styles.scss';
 
-export default class Directory extends Component {
-  constructor() {
-    super();
+const Directory = ({ sections }) => (
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherProps }) => (
+      <MenuItem key={id} {...otherProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      sections: [
-        {
-          title: 'lenses',
-          imgUrl: 'https://i.imgur.com/UiHbkxu.jpg',
-          pageUrl: '',
-          id: 1
-        },
-        {
-          title: 'film',
-          imgUrl: 'https://i.imgur.com/eje9Qe5.jpg',
-          pageUrl: '',
-          id: 2
-        },
-        {
-          title: 'accessories',
-          imgUrl: 'https://i.imgur.com/hossXyM.jpg',
-          pageUrl: '',
-          id: 3
-        },
-        {
-          title: 'digital cameras',
-          imgUrl: 'https://i.imgur.com/29wB4x9.jpg',
-          pageUrl: '',
-          size: 'large',
-          id: 4
-        },
-        {
-          title: 'film cameras',
-          imgUrl: 'https://i.imgur.com/ynqHZLy.jpg',
-          pageUrl: 'filmcameras',
-          size: 'large',
-          id: 5
-        }
-      ]
-    }
-  }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirSections
+});
 
-  render() {
-    return (
-      <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...otherProps }) => (
-            <MenuItem key={id} {...otherProps} />
-        ))}
-      </div>
-    );
-  }
-}
+export default connect(mapStateToProps)(Directory);
