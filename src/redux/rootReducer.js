c29476e9e7f4a4bc1049ac1bedd6d6d27ed05274
David Ordovskiy <david.ordovsky@gmail.com>
2020-08-17 08:10:47 -0400

    bring in redux-persist and start configuring cart persistance

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,25 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
-// the root reducer represents the entire state of our app
 
-export default combineReducers({
+const persistConfig = {
+  // at what point do we want 
+  // to start storing everything
+  key: 'root',
+  storage,
+  // what we're actually storing
+  // (user is stored by firebase, 
+  // so no need for that)
+  whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 });
+
+// export the modified root reducer as default
+export default persistReducer(persistConfig, rootReducer);
