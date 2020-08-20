a0428287ff55163936d75d4cec6e3eb1c3dc1a73
David Ordovskiy <david.ordovsky@gmail.com>
2020-08-20 07:15:30 -0400

    rename category to collection

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collectionItem.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className='collection-page'>
+    <h2>COLLECTION PAGE (under construction)</h2>
+  </div>
+);
+
+export default CollectionPage;
