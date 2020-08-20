9ca36b1b37b6e19c9b12d4cdb4689cb517703684
David Ordovskiy <david.ordovsky@gmail.com>
2020-08-20 07:23:35 -0400

    create selector to grab a collection based on url param

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,26 @@
 import { createSelector } from 'reselect';
 
+const COLLECTION_ID_MAP = {
+  lenses: 1,
+  film: 2,
+  accessories: 3,
+  digitalcameras: 4,
+  filmcameras: 5
+};
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
+
+// the param comes from the URL
+export const selectCollection = collectionParam => {
+  createSelector(
+    [selectCollections],
+    collections => collections.find(
+      collection => collection.id === COLLECTION_ID_MAP[collectionParam]
+    )
+  );
+};
